e2faf9517067244371891ea5874317acc96428a8
4116 Степченко С.А <stepchenkosofia333@gmail.com>
2024-01-03 20:47:32 +0300

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "protei1sprint"
---
+++ src/main/kotlin/com/protei1sprint/Application.kt
@@ -0,0 +1,17 @@
+package com.protei1sprint
+
+import com.protei1sprint.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+            .start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
