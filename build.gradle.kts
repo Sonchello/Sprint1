ad77573255b36edf8d5ab6866f49cda4623be7bc
4116 Степченко С.А <stepchenkosofia333@gmail.com>
2024-01-31 07:45:05 +0300

    add

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,22 @@
 val ktor_version: String by project
 val kotlin_version: String by project
-val logback_version: String = "1.4.12"
+val logback_version: String by project
 
 val postgres_version: String by project
-val h2_version: String = "2.2.220"
+val h2_version: String by project
 val exposed_version: String by project
 plugins {
     kotlin("jvm") version "1.9.21"
     application
-    id("io.ktor.plugin") version "2.3.7"
+    id("io.ktor.plugin") version "2.3.6"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.21"
 }
 
-group = "com.protei1sprint"
+group = "com.proteikotlin"
 version = "0.0.1"
 
 application {
-    mainClass.set("com.protei1sprint.ApplicationKt")
+    mainClass.set("com.proteikotlin.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
@@ -36,11 +36,11 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm:2.3.7")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.3.7")
     implementation("org.postgresql:postgresql:$postgres_version")
-    implementation("com.h2database:h2:2.2.220")
+    implementation("com.h2database:h2:$h2_version")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("io.ktor:ktor-server-netty-jvm:2.3.7")
-    implementation("ch.qos.logback:logback-classic:1.4.12")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:2.3.7")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/com/protei1sprint/plugins/Authentication.kt
+++ src/main/kotlin/com/protei1sprint/plugins/Authentication.kt
@@ -2,18 +2,31 @@ package com.protei1sprint.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
-fun Application.configureAuth(){
+
+data class UserCredentials(val name: String, val password: String)
+
+// Функция для конфигурации аутентификации
+fun Application. configureAuthentication() {
     install(Authentication) {
         basic("auth-basic") {
-            realm = "local host"
-            // sendWithoutRequest { true }
+            realm = "localhost"
+            // Функция для валидации учетных данных
             validate { credentials ->
-                if (credentials.name == "username" && credentials.password == "password") {
-                    UserIdPrincipal(credentials.name)
+                // Проверка, являются ли переданные учетные данные объектом UserPasswordCredential
+                if (credentials is UserPasswordCredential) {
+                    val userCredentials = UserCredentials(credentials.name, credentials.password)// Преобразование UserPasswordCredential в объект Credentials
+                    validateUserCredentials(userCredentials)?.let { UserIdPrincipal(it.name) }// Вызов функции для валидации пользовательских учетных данных  и преобразование результата в UserIdPrincipal
                 } else {
                     null
                 }
             }
         }
+    }
+}
+fun validateUserCredentials(credentials: UserCredentials): UserCredentials? {
+    return if (credentials.name == "username" && credentials.password == "password") {
+        credentials
+    } else {
+        null
     }
 }
